a69d96ffa03c94512aaed688fb2f99cac79f8b9f
Doug <dougstein@gmail.com>
2018-07-18 17:44:51 -0700

    'Cancel' for PromiseKit

--- Sources/SKReceiptRefreshRequest+Promise.swift
+++ Sources/SKReceiptRefreshRequest+Promise.swift
@@ -1,4 +1,5 @@
 #if !PMKCocoaPods
+import PMKCancel
 import PromiseKit
 #endif
 import StoreKit
@@ -9,7 +10,7 @@ extension SKReceiptRefreshRequest {
     }
 }
 
-private class ReceiptRefreshObserver: NSObject, SKRequestDelegate {
+private class ReceiptRefreshObserver: NSObject, SKRequestDelegate, CancellableTask {
     let (promise, seal) = Promise<SKReceiptRefreshRequest>.pending()
     let request: SKReceiptRefreshRequest
     var retainCycle: ReceiptRefreshObserver?
@@ -31,5 +32,22 @@ private class ReceiptRefreshObserver: NSObject, SKRequestDelegate {
     func request(_: SKRequest, didFailWithError error: Error) {
         seal.reject(error)
         retainCycle = nil
+    }
+
+    var isCancelled = false
+    
+    func cancel() {
+        request.cancel()
+        retainCycle = nil
+        isCancelled = true
+    }
+}
+
+//////////////////////////////////////////////////////////// Cancellation
+
+extension SKReceiptRefreshRequest {
+    public func promiseCC() -> CancellablePromise<SKReceiptRefreshRequest> {
+        let rro = ReceiptRefreshObserver(request: self)
+        return CancellablePromise(task: rro, promise: rro.promise, resolver: rro.seal)
     }
 }
